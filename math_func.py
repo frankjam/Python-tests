985d5e2923639938dd5fa96c55d2844e04da06d3
frankjam <frank.jamlick@gmail.com>
2020-03-01 20:46:08 +0300

    starting point

--- math_func.py
+++ math_func.py
@@ -1,2 +1,2 @@
-def math_func(x,y):
+def add(x,y):
     return  x + y
--- test_math_func.py
+++ test_math_func.py
@@ -1,3 +1,5 @@
 import  math_func
 
-
+def test_add():
+    assert math_func.add(7,3) == 10
+    assert  math_func.add(-1,2)== 0
